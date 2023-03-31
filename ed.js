0f98850df6e9de91e8ba4ec416f3a29ff5123f79
jekr <48930765+a7mdal3le@users.noreply.github.com>
2023-03-31 04:22:28 +0300

    Create ed.js

---
+++ ed.js
@@ -0,0 +1,9 @@
+$(`<style>.nosel.d-flex.room.r1868352f68ex91308303-xhn9hks.bord {border-inline: 4px solid #6c6c6c !important;}.nosel.d-flex.room.r1868352f68ex91308303-xhn9hks {display: inline-block;background-image: linear-gradient(-225deg, rgb(202, 202, 202) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(198, 198, 198) 100%);border-radius: 0px 0px 15px 15px;}.nosel.d-flex.room.r1868352f68ex91308303-xhn9hks .dots.mini.u-msg {text-align: center;background-size: 200% auto; -webkit-background-clip: text;-webkit-text-fill-color: #0000;background-image: linear-gradient(225deg,#000 0%,#000 29%,#868686 67%,#131212 100%);}.nosel.d-flex.room.r1868352f68ex91308303-xhn9hks .fitimg.u-pic.borderg{border-radius: 100px;border: 2px solid #a4a4a4;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;}
+.nosel.d-flex.room.r1868352f68ex91308303-xhn9hks .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
+letter-spacing: 1px;
+-webkit-text-fill-color: #000;
+padding: 3px !important;
+border-radius: 9px !important;
+margin-top: -30px !important;
+background-color: #d9534f00;}
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
